0468da8716af4fcfccc399e02835ae59d06881dd
Smooke09 <pedrolucas1014zz@gmail.com>
2022-06-10 10:51:49 -0300

    challenge function merge

--- src/views/examples/UseRef.jsx
+++ src/views/examples/UseRef.jsx
@@ -10,10 +10,31 @@ const UseRef = (props) => {
   useEffect(
     function () {
       count.current++;
+      myInput2.current.focus();
     },
     [value1]
   );
   // exit <-
+
+  //Exercico #02 ->
+  const [value2, setValue2] = useState("");
+  const myInput1 = useRef(null);
+  const myInput2 = useRef(null);
+
+  useEffect(
+    function () {
+      count.current++;
+      myInput1.current.focus();
+    },
+    [value2]
+  );
+  // <-
+
+  //Desafio Funcao merger
+  const merge = function (s1, s2) {
+    return [...s1].map((e, i) => `${e}${s2[i] || ""}`).join("");
+  };
+
   return (
     <div className="UseRef">
       <PageTitle
@@ -24,7 +45,7 @@ const UseRef = (props) => {
       <div className="center">
         <div>
           <span className="text">Valor: </span>
-          <span className="text">{value1}[</span>
+          <span className="text">{merge(value1, value2)}[</span>
           <span className="text red">{count.current}</span>
           <span className="text">]</span>
         </div>
@@ -32,8 +53,19 @@ const UseRef = (props) => {
         <input
           type="text"
           className="input"
+          ref={myInput1}
           value={value1}
           onChange={(e) => setValue1(e.target.value)}
+        />
+      </div>
+      <SectionTitle title="Exercico #02" />
+      <div className="center">
+        <input
+          type="text"
+          className="input"
+          ref={myInput2}
+          value={value2}
+          onChange={(e) => setValue2(e.target.value)}
         />
       </div>
     </div>
