00da81b9a37e88c0b18d07449220d54b00a0f1c7
Smooke09 <pedrolucas1014zz@gmail.com>
2022-06-10 09:40:06 -0300

    Exercise useEffect

--- src/views/examples/UseEffect.jsx
+++ src/views/examples/UseEffect.jsx
@@ -1,15 +1,78 @@
-import React from 'react'
-import PageTitle from '../../components/layout/PageTitle'
+import React, { useEffect } from "react";
+import PageTitle from "../../components/layout/PageTitle";
+import { useState } from "react";
+import SectionTitle from "../../components/layout/SectionTitle";
 
-const UseEffect = (props) => {
-    return (
-        <div className="UseEffect">
-            <PageTitle
-                title="Hook UseEffect"
-                subtitle="Permite executar efeitos colaterais em componentes funcionais!"
-            />
-        </div>
-    )
+function calcFatorial(num) {
+  const n = parseInt(num);
+  if (n < 0) return -1;
+  if (n === 0) return 1;
+  return calcFatorial(n - 1) * n;
 }
 
-export default UseEffect
+const UseEffect = (props) => {
+  const [number, setNumber] = useState(1);
+  const [fatorial, setFatorial] = useState(1);
+
+  useEffect(
+    function () {
+      setFatorial(calcFatorial(number));
+    },
+    [number]
+  );
+
+  useEffect(
+    function () {
+      if (fatorial > 1000000) {
+        document.title = "Eita!!!";
+      }
+    },
+    [fatorial]
+  );
+
+  return (
+    <div className="UseEffect">
+      <PageTitle
+        title="Hook UseEffect"
+        subtitle="Permite executar efeitos colaterais em componentes funcionais!"
+      />
+      <SectionTitle title="Exercicio #01" />
+      <div className="center">
+        <div>
+          <span className="text">Fatorial: </span>
+          <span className="text red">
+            {fatorial === -1 ? "Nao existe" : fatorial}
+          </span>
+        </div>
+        <input
+          type="number"
+          className="input"
+          value={number}
+          onChange={(e) => setNumber(e.target.value)}
+        />
+      </div>
+
+      <SectionTitle title="Exercicio #02" />
+      <div className="center">
+        <div>
+          <span
+            className="text"
+            style={{
+              fontSize: "1.8rem",
+            }}
+          >
+            Par Ou Impar:
+          </span>
+        </div>
+        <input
+          type="text"
+          className="input center"
+          value={number}
+          onChange={(e) => setNumber(e.target.value)}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default UseEffect;
